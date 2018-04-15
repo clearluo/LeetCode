71a89de9254a7022632391a9e95a5523ab29dfb5
clearluo <tolsw@qq.com>
2018-04-15 18:48:29 +0800

    add leetcode

---
+++ p0001/p0001.go
@@ -0,0 +1,13 @@
+package p0001
+
+func twoSum(nums []int, target int) []int {
+	l := len(nums)
+	m := make(map[int]int, l)
+	for i := 0; i < l; i++ {
+		if j, ok := m[target-nums[i]]; ok {
+			return []int{j, i}
+		}
+		m[nums[i]] = i
+	}
+	return nil
+}
---
+++ p0007/p0007.go
@@ -0,0 +1,16 @@
+package p0007
+
+import "math"
+
+func reverse(x int) int {
+	var ret int64
+	for x != 0 {
+		ret = ret*10 + int64(x%10)
+		x = x / 10
+	}
+
+	if ret < math.MinInt32 || ret > math.MaxInt32 {
+		return 0
+	}
+	return int(ret)
+}
