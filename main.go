6100cf3db73e061c81ce066a9b0f9b68fcf83b17
clearluo <tolsw@qq.com>
2018-04-24 08:44:18 +0800

    add p0027

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"LeetCode/p0026"
+	"LeetCode/p0027"
 	"fmt"
 )
 
 func main() {
-	data := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
-	l := p0026.RemoveDuplicates(data)
+	data := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	l := p0027.RemoveElement(data, 2)
 	fmt.Println("l:", l)
 	for i := 0; i < l; i++ {
 		fmt.Println(data[i])
---
+++ p0027/p0027.go
@@ -0,0 +1,16 @@
+package p0027
+
+func RemoveElement(nums []int, val int) int {
+	l := len(nums)
+	res := 0
+	for i := 0; i < l; i++ {
+		if nums[i] == val {
+			continue
+		}
+		if res != i {
+			nums[res] = nums[i]
+		}
+		res++
+	}
+	return res
+}
